d832b2bf79affdef0f1f6746923ae9ba67b8b73d
dak200423 <166195026+dak200423@users.noreply.github.com>
2024-05-23 01:53:50 +0000

    dynamic complete

---
+++ app/controllers/dynamic_controller.rb
@@ -0,0 +1,16 @@
+class DynamicController < ApplicationController
+  def elephant
+    @num_dice = params.fetch("alice").to_i
+ 
+    @sides = params.fetch("bob").to_i
+    
+     @rolls = []
+   
+     @num_dice.times do
+       die = rand(1..@sides)
+   
+       @rolls.push(die)
+     end
+     render({ :template => "roll_dices/dynamic"})
+  end
+end
