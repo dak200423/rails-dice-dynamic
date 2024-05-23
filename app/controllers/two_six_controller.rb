11fd602afe6554f662086a3107b23ddfc7585c28
dak200423 <166195026+dak200423@users.noreply.github.com>
2024-05-23 01:45:09 +0000

    two_six complete

---
+++ app/controllers/two_six_controller.rb
@@ -0,0 +1,13 @@
+class Two_SixController < ApplicationController
+  def lion
+    @rolls = []
+
+  2.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+    render({ :template => "roll_dices/two_six"})
+  end
+end
