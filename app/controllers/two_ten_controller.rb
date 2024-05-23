b42345f60f30230a1e296ea9b3d589c2918fa634
dak200423 <166195026+dak200423@users.noreply.github.com>
2024-05-23 01:47:00 +0000

    two_ten complete

---
+++ app/controllers/two_ten_controller.rb
@@ -0,0 +1,13 @@
+class Two_TenController < ApplicationController
+  def zebra
+    @rolls = []
+
+    2.times do
+      die = rand(1..10)
+  
+      @rolls.push(die)
+    end
+
+    render({ :template => "roll_dices/two_ten"})
+  end
+end
